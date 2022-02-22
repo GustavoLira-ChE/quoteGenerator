d43d6c0fee8be1298af5d82391f5d4373300c5bf
Gustavo Lira <gu.lirar23@gmail.com>
2022-02-22 11:12:29 -0600

    feat: tweet intent added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,7 @@ class App extends React.Component{
             author: '',
         }
         this.handleQuotes = this.handleQuotes.bind(this);
+        this.changeBkgColor = this.changeBkgColor.bind(this);
     }
     componentDidMount(){
         this.handleQuotes()
@@ -28,21 +29,35 @@ class App extends React.Component{
                 })
             }
         });
-
+    }
+    changeBkgColor(){
+        let color = chooseColor();
+        return color
     }
     render(){
+        let color = this.changeBkgColor()
+        let url = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+      encodeURIComponent('"' + this.state.quote + '" ' + this.state.author)
         return(
-            <div id="quote-box">
-                <h2 id="text">{this.state.quote}</h2>
-                <p id="author" >{this.state.author}</p>
-                <div>
-                    <button>
-                        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
-                        </a>
-                        <i className="bi bi-twitter"></i>
-                    </button>
-                    <button id="new-quote" type="button" className="btn btn-primary" onClick={this.handleQuotes}>Get new quote</button>
+            <div id="bkg-color" style={{backgroundColor: color}}>
+                <div id="quote-box">
+                    <div>
+                        <i id="float-element" className="bi bi-quote" style={{color: color}}></i>
+                        <h2 id="text" onChange={this.changeBkgColor} style={{color: color}}>{this.state.quote}</h2>
+                    </div>
+                    <h4 id="author" style={{color: color}} >{this.state.author}</h4>
+                    <div id="button-sec">
+                        <button id="tweet-quote" type="button" className="btn btn-primary" style={{backgroundColor: color}}>
+                            <a href={url} target="_blank">
+                                <i id="tweet-icon" className="bi bi-twitter"></i>
+                            </a>
+                        </button>
+                        <button id="new-quote" type="button" className="btn btn-primary" onClick={this.handleQuotes} style={{backgroundColor: color}}>
+                            Get new quote
+                        </button>
+                    </div>
                 </div>
+                <p style={{color:'white'}}>by Gustavo Lira</p>
             </div>
         )
     }
@@ -66,5 +81,24 @@ async function fetchApiQuotes(){
 }
 function randomNumbers(maxNumber){
     return Math.floor(Math.random()*maxNumber)
+}
+function chooseColor (){
+    const colors = [
+        '#16a085',
+        '#27ae60',
+        '#2c3e50',
+        '#f39c12',
+        '#e74c3c',
+        '#9b59b6',
+        '#FB6964',
+        '#342224',
+        '#472E32',
+        '#BDBB99',
+        '#77B1A9',
+        '#73A857'
+      ];
+    let numberOfColor = colors.length
+    let color = colors[randomNumbers(numberOfColor)]
+    return color
 }
 export default App;
